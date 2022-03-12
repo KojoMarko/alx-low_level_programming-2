2ea4cc3f0bdf719385a5712566085c71261a3361
jayamon99 <henryquaye99@outlook.com>
2022-03-12 02:30:22 +0000

    program that prints the alphabet in lowercase with exceptions

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-    char i;
+	char i;
 
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        if (i == 'e' || i == 'q')
-            continue;
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
+
