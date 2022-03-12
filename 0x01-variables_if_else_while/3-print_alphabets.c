8758b3030017fcfe5c019dff93a7d04f08694a17
Henry Quaye <henryquaye99@outlook.com>
2022-03-12 00:48:34 +0000

    Print alphabets in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,21 +2,22 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char i;
+	char i;
 
-        for (i = 'a'; i <= 'z'; i++)
-        {
-            putchar(i);
-        }
-        for (i = 'A'; i <= 'Z'; i++)
-        {
-            putchar(i);
-        }
-        putchar('\n');
-        return 0;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
