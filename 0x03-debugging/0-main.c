1ce5ef7ab81b1edf54212cba4d235d3b13d32357
jayamon99 <henryquaye99@outlook.com>
2022-03-18 00:58:54 +0000

    Test for function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * postitive_or_negative - checks whether a number is zero or positive or negative
+ *
+ * @i - argument for function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 99;
+	positive_or_negative(i);
+
+	return (0);
+}
+
