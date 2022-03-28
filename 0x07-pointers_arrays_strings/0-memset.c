db2b3afe654ec8493b6c88b92db753c651d3b9f1
jayamon99 <henryquaye99@outlook.com>
2022-03-28 22:56:08 +0000

    error fix: code indentations

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * _memset - functions that fills first n bytes of memory
- * pointed to by pointer with b;
+ * pointed to by pointer with b
  * @s: pointer to char
  * @b: function argument
  * @n: function argument
@@ -11,11 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; i < n; i++)
-        {
-            *(s + i) = b;
-        }
-        return (s);
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
 }
