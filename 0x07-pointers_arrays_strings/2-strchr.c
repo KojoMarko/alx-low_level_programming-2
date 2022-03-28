2a660ada7e145c256fb82c98946066ea308743e4
jayamon99 <henryquaye99@outlook.com>
2022-03-28 23:53:26 +0000

    function that finds character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; *s != '\0'; i++)
+	for (i = 0; *s; i++)
 	{
-		if (c == *(s + i))
+		if (*(s + i) == c)
 		{
 			return ((s + i));
 		}
 	}
-	return ((s + i));
+	return (0);
 }
